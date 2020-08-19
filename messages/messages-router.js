1cce009c6112c7e975218046e964e686c4f92122
Michelle <Michelle.Tinajero@outlook.com>
2020-08-19 12:14:00 -0500

    notes

--- messages/messages-router.js
+++ messages/messages-router.js
@@ -3,16 +3,55 @@ const db = require("../data/config")
 
 const router = express.Router()
 
-router.get("/", (req, res, next) => {
+router.get("/", async (req, res, next) => {
+    try {
+        //SQL Command: SELECT * FROM "Messages";
+        const messages = await db.select("*").from("messages")
+
+        res.json(messages)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try { //translates to SELECT * FROM messages WHERE id=?;
+        const [message] = await db
+        .select("*")
+        .from("messages")
+        .where("id", req.params.id)
+        .limit(1)
+
+        res.json(message)
+    } catch (err) {
+        next(err)
+    }
 
 })
 
-router.get("/:id", (req, res, next) => {
+router.post("/", async (req, res, next) => {
+    try {
+        //translates to INSERT INTO messages (title, contents) VALUES (?, ?);
+        const [id] = await db
+            .insert({
+                //database will automatically generate the id
+                title: req.body.title,
+                contents: req.body.contents,
+            })
+            .into("messages")
 
-})
+            //translates to SELECT * FROM messages WHERE id = ? LIMIT 1;
+            const message = await db
+            .select("*")
+            .from("messages")
+            .where("id", id)
+            .first() //converts to limit 1
 
-router.post("/", (req, res, next) => {
+            res.status(201).json(message)
 
+    } catch (er) {
+        next(err)
+    }
 })
 
 router.put("/:id", (req, res, next) => {
